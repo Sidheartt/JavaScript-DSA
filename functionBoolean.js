2f65cf737cda2c6dd9b38892c6826ab033cb1f21
Siddharth Desai <75911033+Sidheartt@users.noreply.github.com>
2021-05-19 00:23:19 +0530

    initial commit

---
+++ functionBoolean.js
@@ -0,0 +1,7 @@
+// Advance js function that returns true if 
+// the provided predicate function returns true for all elements in Collection,
+// else returns false.
+
+
+console.log([1,2,3,4,5,6].every(x => x > 0))
+console.log([1,2,3,4,5,6].every(x => x > 3))
