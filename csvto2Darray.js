be0a03b1b1a86625c1b6012d77916b37672940ac
Siddharth Desai <75911033+Sidheartt@users.noreply.github.com>
2021-05-18 23:59:50 +0530

    initial commit
    csv to 2D ARRAY

---
+++ csvto2Darray.js
@@ -0,0 +1,10 @@
+// JavaScript program to convert a csv string to a 2D array
+
+const parseCSV = (csvString) => 
+csvString.split('\n').map(row=> row.split(','));
+
+const str = 'abc, def, ghi
+jkl, mno, pqr
+stu, vwx, yza';
+
+console.log(parseCSV(str));
