fa158b49e251478847b95546bbc0c539b7592e89
Siddharth Desai <75911033+Sidheartt@users.noreply.github.com>
2021-05-19 21:13:28 +0530

    initial commit
    Javascript function to extract unique characters from a string

---
+++ uniqueChar.js
@@ -0,0 +1,12 @@
+// Javascript function to extract unique characters from a string
+
+const getUniqueChars = (str) => 
+str.split('').filter(
+    (item, index, arr) =>
+     arr.slice(index + 1).indexOf(item) === -1 
+     );
+
+    console.log(getUniqueChars('aaaabbbbbbcccc'));
+    console.log(getUniqueChars('aaaa94hhhhhhhhhhhfffgggggggrrrreettteewwqwwwwwwhhhh'));
+
+  
